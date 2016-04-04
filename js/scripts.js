a7a21489833d44ef3fcab4a70cc8fbb55a737881
Blake Bless and Nadiya Yeroshkina <blake@>
2016-04-04 14:16:02 -0700

    made to-do list that takes input and organizes it into a list. yo

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,22 @@
 //business logic
-function Places(Name, Landmark, TimeOfYear, Notes) {
-  this.inputName = Name;
-  this.inputLandmark = Landmark;
-  this.inputTimeOfYear = TimeOfYear;
-  this.inputNotes = Notes;
+function toDo(Name) {
+  this.inputtoDo = Name;
 }
 
 // user interface logic
 $(document).ready(function() {
-  $("form#new-place").submit(function(event) {
+  $("form#new-toDo").submit(function(event) {
     event.preventDefault();
 
 
-    var inputtedName = $("input#place-one").val();
-    var inputtedLandmarks = $("input#place-two").val();
-    var inputtedYear = parseInt($("input#place-three").val());
-    var inputtedNotes = $("input#place-four").val();
+    var inputtedThing = $("input#thing").val();
 
-    var newPlace = new Places(inputtedName, inputtedLandmarks, inputtedYear, inputtedNotes);
-    $("ul#list").append("<li><span class = 'rightSide'>" + newPlace.inputName + "</span></li>");
+    var newThing = new toDo(inputtedThing);
+    $("ul#list").append("<li>" + newThing.inputtoDo + "</li>");
+    console.log($("ul#list li").last().text());
+    $("ul#list li").click(function() {
+      $(this).hide();
 
-    $("ul#list").last().click(function() {
-      $("#show-place").show();
-      $("#show-place h2").text(newPlace.inputName);
-      $(".inputtedName").text(newPlace.inputName);
-      $(".inputtedLandmarks").text(newPlace.inputLandmark);
-      $(".inputtedYear").text(newPlace.inputTimeOfYear);
-      $(".inputtedNotes").text(newPlace.inputNotes);
     });
-
-    // $("input#place-one").val("");
-    // $("input#place-two").val("");
-    // parseInt($("input#place-three").val(""));
-    // $("input#place-four").val("");
   });
 });
