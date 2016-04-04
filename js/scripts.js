8d437be192a2ef96f01df203590b3f2abcd01c65
Blake Bless and Nadiya Yeroshkina <blake@>
2016-04-04 11:29:46 -0700

    made Favorite Places page with funtional js logic and interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,37 @@
 //business logic
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
+function Places(Name, Landmark, TimeOfYear, Notes) {
+  this.inputName = Name;
+  this.inputLandmark = Landmark;
+  this.inputTimeOfYear = TimeOfYear;
+  this.inputNotes = Notes;
 }
 
 // user interface logic
 $(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+  $("form#new-place").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var inputtedName = $("input#place-one").val();
+    var inputtedLandmarks = $("input#place-two").val();
+    var inputtedYear = parseInt($("input#place-three").val());
+    var inputtedNotes = $("input#place-four").val();
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    var newPlace = new Places(inputtedName, inputtedLandmarks, inputtedYear, inputtedNotes);
+    $("ul#list").append("<li><span class = 'rightSide'>" + newPlace.inputName + "</span></li>");
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
+    $("ul#list").last().click(function() {
+      $("#show-place").show();
+      $("#show-place h2").text(newPlace.inputName);
+      $(".inputtedName").text(newPlace.inputName);
+      $(".inputtedLandmarks").text(newPlace.inputLandmark);
+      $(".inputtedYear").text(newPlace.inputTimeOfYear);
+      $(".inputtedNotes").text(newPlace.inputNotes);
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+    // $("input#place-one").val("");
+    // $("input#place-two").val("");
+    // parseInt($("input#place-three").val(""));
+    // $("input#place-four").val("");
   });
 });
